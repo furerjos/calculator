b1377b00510c62d904bc2ba00d13a42ae381ee9b
harriszk <zacharykeatonharris@gmail.com>
2023-09-15 14:37:28 -0400

    Added comments to CalculatorService

--- src/main/java/Service/CalculatorService.java
+++ src/main/java/Service/CalculatorService.java
@@ -1,19 +1,53 @@
 package Service;
 
 public class CalculatorService {
+    /**
+     * Default constructor.
+     */
+    public CalculatorService() {
 
+    }
+
+    /**
+     * Adds two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The sum of the two numbers.
+     */
     public double add(double a, double b) {
         return a + b;
     }
 
+    /**
+     * Subtracts two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The difference of the two numbers.
+     */
     public double subtract(double a, double b) {
         return a - b;
     }
 
+    /**
+     * Multiplies two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The result of the two numbers multiplied together.
+     */
     public double multiply(double a, double b) {
         return a * b;
     }
 
+    /**
+     * Divides two numbers.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The result of the two numbers divided.
+     */
     public double divide(double a, double b) {
         return a / b;
     }
