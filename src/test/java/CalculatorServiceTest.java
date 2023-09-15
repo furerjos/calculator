b5035a1dcaf4c170ae415475cf852810b772ebb2
Joshua M Furer <furerjos@shu.edu>
2023-09-15 14:20:48 -0400

    Base Layer 2.0

--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class CalculatorServiceTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorServiceTest {
+
+
+    @Test
+    public void AdditionTest1(){
+        CalculatorService service = new CalculatorService();
+        double a = 5;
+        double b = 10;
+        double expected = 15;
+        double actual = service.add(a, b);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void SubtractionTest1(){
+        CalculatorService service = new CalculatorService();
+        double a = 5;
+        double b = 10;
+        double expected = 5;
+        double actual = service.subtract(b, a);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void MultiplicationTest1(){
+        CalculatorService service = new CalculatorService();
+        double a = 5;
+        double b = 10;
+        double expected = 150;
+        double actual = service.multiply(a, b);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void DivisionTest1(){
+        CalculatorService service = new CalculatorService();
+        double a = 5;
+        double b = 10;
+        double expected = 2;
+        double actual = service.divide(b, a);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
