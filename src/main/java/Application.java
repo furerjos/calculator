2ba832f534229c40a0ced7fdac445acd0a490cd3
Joshua M Furer <furerjos@shu.edu>
2023-09-15 15:02:24 -0400

    First Working App

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,6 +8,9 @@ public class Application {
 
         while (true) {
             System.out.println("Welcome to the world famous calculator app!");
+            System.out.println("In order to use the calculator app, expect to " +
+                    "input one number at a time after choosing your operation.");
+
             System.out.println("Choose one of the following operations:");
             System.out.println("1: Add");
             System.out.println("2: Subtract");
@@ -25,6 +28,32 @@ public class Application {
             double num1 = scanner.nextDouble();
             System.out.print("Enter the second number:");
             double num2 = scanner.nextDouble();
+
+            double result;
+
+            switch (choice) {
+                case 1:
+                    result = calculator.add(num1, num2);
+                    break;
+                case 2:
+                    result = calculator.subtract(num1, num2);
+                    break;
+                case 3:
+                    result = calculator.multiply(num1, num2);
+                    break;
+                case 4:
+                    if (num2 != 0) {
+                        result = calculator.divide(num1, num2);
+                    } else {
+                        System.out.println("Error: Division by zero");
+                        continue;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please choose an input between 1 - 5.");
+                    continue;
+            }
+            System.out.println("Result: " + result);
         }
         scanner.close();
     }
